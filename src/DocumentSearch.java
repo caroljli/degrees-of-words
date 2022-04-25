d129231a4cf6e42dbe3a9a342d46c01a596efcc5
Carol Li <caroldotli@gmail.com>
2022-04-25 13:57:27 -0400

    Add Jsoup

--- src/DocumentSearch.java
+++ src/DocumentSearch.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DocumentSearch {
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class DocumentSearch {
+  public DocumentSearch() {
+
+  }
 }
