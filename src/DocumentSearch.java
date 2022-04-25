451ec23af4915adb85ca651a43da30fcb31ae378
Carol Li <caroldotli@gmail.com>
2022-04-25 14:06:53 -0400

    Add skeleton functions

--- src/DocumentSearch.java
+++ src/DocumentSearch.java
@@ -9,9 +9,36 @@ import org.jsoup.select.Elements;
 
 public class DocumentSearch {
   private Document document;
-  final String PLACEHOLDER = "https://www.britannica.com/animal/jellyfish";
-  
-  public DocumentSearch() {
+  final String PLACEHOLDER_URL = "https://www.britannica.com/animal/jellyfish";
+  final String PLACEHOLDER_TERMS = "algae";
 
+  private Map<String, String> allLinks = new HashMap<>();
+  private Map<String, List<String>> outputMap = new HashMap<>();
+
+  public DocumentSearch() {
+    // TODO
+  }
+
+  public void getAllLinks() {
+    // TODO
+  }
+
+  public boolean searchLinks(String term) {
+    // TODO
+    return true;
+  }
+
+  public int getTfIdf() {
+    // TODO
+    return 0;
+  }
+
+  public int getCosineSimilarity() {
+    // TODO
+    return 0;
+  }
+
+  public Map<String, List<String>> getResults() {
+    return this.outputMap;
   }
 }
