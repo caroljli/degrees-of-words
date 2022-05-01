8f65715287229ce37211f3b77e975e3e07376724
Carol Li <caroldotli@gmail.com>
2022-05-01 15:29:34 -0400

    Create scanner for main

--- src/BritannicaMain.java
+++ src/BritannicaMain.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class BritannicaMain {
 
     public static void main (String[] args) {
-        DocumentSearch test = new DocumentSearch();
-        test.getAllLinks();
-        test.createTfIdf();
-        test.createCosineSimilarity();
+        DocumentSearch documentSearch = new DocumentSearch();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input the Brittanica page that you want to search.");
+        String brittanicaInput = scanner.nextLine();
+
+        System.out.println("Please intput the query text.");
+        String query = scanner.nextLine();
+
+        documentSearch.getAllLinks();
+        documentSearch.createTfIdf();
+        documentSearch.createCosineSimilarity();
 
 //        System.out.println(test.getTfIdfWeights());
 //        System.out.println(test.getCosineSimilarities());
